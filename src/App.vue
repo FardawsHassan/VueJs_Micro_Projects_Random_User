a17750659dfa4b65309568a1139ba6c1f1a49edb
FardawsHassan <fardawshassanshuvro@gmail.com>
2022-03-04 03:54:20 +0600

    Primary upload

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <UserInfo></UserInfo>
+</template>
+
+<script>
+import UserInfo from './components/user.vue'
+
+export default {
+  name: 'App',
+  components: {
+     UserInfo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
