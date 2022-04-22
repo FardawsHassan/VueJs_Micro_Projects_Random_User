4c1ad2c1b6cbf4c5315b3b7032838f26c4488b14
FardawsHassan <fardawshassanshuvro@gmail.com>
2022-04-22 16:45:17 +0600

    fetch data updated

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <UserInfo :getdata="getdata"></UserInfo>
+  <UserInfo :getdata="userdata"></UserInfo>
 </template>
 
 <script>
 import UserInfo from './components/user.vue'
+// import vue from 'vue'
+// import axios from 'axios'
+// import VueAxios from 'vue-axios'
+// vue.use(VueAxios.axios)
 
 export default {
   name: 'App',
@@ -12,20 +16,46 @@ export default {
   },
   data(){
     return{
-      data:null,
-      userdata: null
+      dataList:[],
+      user: {
+        name:null,
+        gender:null,
+        phone:null,
+        cell:null,
+        email:null,
+        picture:null,
+        dob:null,
+        location:null
+      }
     }
   },
   methods: {
-    getdata: function(){
-      console.log("hello")
-      this.userdataa()
+    async getdata(){
+      const res = await fetch("https://randomuser.me/api");
+      const{ results } = await res.json()
+      var d = results[0]
+
+      this.user.name = d.name
+      this.user.gender = d.gender
+      this.user.phone = d.phone
+      this.user.cell = d.cell
+      this.user.email = d.email
+      this.user.picture = d.picture
+      this.user.dob = d.dob
+      this.user.location = d.location
+      console.log(this.user)
+      this.dataList.push(this.user)
     },
-    async userdataa(){
-      console.log("haas")
-      const res = await fetch("https://randomuser.me/api/")
-      //this.userdata = res.json()
-      console.log(res)
+    userdata(){
+      this.getdata()
+      this.getdata()
+      this.getdata()
+      this.getdata()
+      console.log(this.dataList)
+      //console.log(this.getdata().gender)
+      //this.dataa.push(this.getdata())
+      //console.log(this.dataa[0])
+      //console.log(JSON.parse(JSON.stringify(this.dataa[0])).gender)
     }
   }
 }
